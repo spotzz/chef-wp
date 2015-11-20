7084c58b7dc72c0402aaf878f19d4542dfe51b86
spotzz <amy.marrich@rackspace.com>
2015-11-20 15:13:08 +0000

    made the defaults into more of a tutorial

--- cookbooks/wp/attributes/default.rb
+++ cookbooks/wp/attributes/default.rb
@@ -1,13 +1,15 @@
 # Wordpress config
 
-default['wordpress']['db']['root_password'] = 'mysql123'
-default['wordpress']['db']['host'] ='104.130.255.127' 
+default['wordpress']['db']['root_password'] = '<mysql root password>'
+default['wordpress']['db']['host'] ='<database loadbalancer IP>'
 default['wordpress']['db']['instance_name'] = 'default'
-default['wordpress']['db']['pass'] = 'asdw231'
-default['wordpress']['db']['user'] = 'wordpress'
+default['wordpress']['db']['pass'] = '<wordpress db password>'
+default['wordpress']['db']['user'] = '<wordpress user>'
 
 # Mysql - Multi
 
-default['mysql-multi']['server_repl_password'] = 'rep123'
+default['mysql-multi']['server_repl_password'] = '<replication password>'
 default['mysql-multi']['service_name'] = 'default'
-default['mysql-multi']['server_root_password'] = 'mysql123'   
+default['mysql-multi']['server_root_password'] = '<mysql root password>'
+
+
